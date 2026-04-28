8fc61a525df283c708e22c84ad210b3c6c280d6b
nwu1015 <marchenkov3004@gmail.com>
2026-04-28 15:52:59 +0300

    Assignment12 v1

--- src/main/java/com/shpp/p2p/cs/vmarchenko/assignment12/Assignment12Part1.java
+++ src/main/java/com/shpp/p2p/cs/vmarchenko/assignment12/Assignment12Part1.java
@@ -10,10 +10,30 @@ import java.util.Map;
 import java.util.Random;
 import java.util.stream.Collectors;
 
+/**
+ * Assignment 12: Silhouettes
+ * Takes the file name as arguments and
+ * determines how many separate silhouettes are in the image
+ *
+ */
+
 public class Assignment12Part1 {
 
-    private static final int PIXELS_TO_DEFINE_BACKGROUND = 50;
+    /**
+     * How many points will be taken to determine the background
+     */
+    private static final int PIXELS_TO_DEFINE_BACKGROUND = 15;
+
+    /**
+     * How much can the average RGB value deviate
+     */
     private static final double CALCULATION_MISTAKE = 10;
+
+    /**
+     * To determine the minimum silhouette size.
+     * Required to avoid unnecessary waste
+     */
+    private static final double NOISE_TOLERANCE = 0.0005;
 
     public static void main(String[] args) {
         Assignment12Part1 assignment = new Assignment12Part1();
@@ -31,10 +51,14 @@ public class Assignment12Part1 {
         assignment.defineBackground(image, isBackground);
 
         System.out.println(assignment.findSilhouettes(image, isVisited, isBackground));
-
-
     }
 
+    /**
+     * It takes the file name and searches for that file in the resources folder.
+     *
+     * @param imageURL file name
+     * @return a file with the appropriate name
+     */
     private BufferedImage getImageFromArgs(String imageURL) {
         try {
             File file = new File(imageURL);
@@ -43,24 +67,35 @@ public class Assignment12Part1 {
             if(file.exists()) {
                 image = ImageIO.read(file);
             }else {
-                InputStream var = Assignment12Part1.class.getResourceAsStream("/" + imageURL);
+                InputStream name = Assignment12Part1.class.getResourceAsStream("/" + imageURL);
 
-                if (var == null) {
+                if (name == null) {
                     throw new Exception("File doesn't exists: " + imageURL);
                 }
 
-                image = ImageIO.read(var);
+                image = ImageIO.read(name);
             }
-
             return image;
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
+    /**
+     * A method for finding silhouettes.
+     * It goes through all pixels and
+     * if it is not the background and the pixel has not been visited -
+     * the recursive depth-first search algorithm is started
+     *
+     * @param image a file with the appropriate name
+     * @param isVisited an array that shows whether the pixel has already been visited
+     * @param isBackground an array indicating whether a pixel belongs to the background
+     * @return number of silhouettes found
+     */
     private int findSilhouettes(BufferedImage image, boolean[][] isVisited, boolean[][] isBackground) {
         int silhouetteCounter = 0;
-        double minSize = (image.getWidth() * image.getHeight()) * 0.01;
+        double minSize = (image.getWidth() * image.getHeight()) * NOISE_TOLERANCE;
 
         for(int y = 0; y < image.getHeight(); y++) {
             for(int x = 0; x < image.getWidth(); x++) {
@@ -78,6 +113,14 @@ public class Assignment12Part1 {
         return silhouetteCounter;
     }
 
+    /**
+     * A method for determining whether a pixel belongs to the background.
+     * Random points are taken, and the most are compared.
+     * Which color is the most - this is the background.
+     *
+     * @param image a file with the appropriate name
+     * @param isBackground an array indicating whether a pixel belongs to the background
+     */
     private void defineBackground(BufferedImage image, boolean[][] isBackground) {
         int number;
         int[] randomWidth = new int[PIXELS_TO_DEFINE_BACKGROUND];
@@ -106,6 +149,14 @@ public class Assignment12Part1 {
         }
     }
 
+    /**
+     * Fills an array with random coordinates within a given range.
+     * To increase the accuracy of background detection,
+     * every fifth element of the array is set as a threshold value (0 or extreme point).
+     *
+     * @param array Array to fill with coordinates
+     * @param number Upper limit of the range (width or height of the image), exclusively
+     */
     private void fillTheArrayWithRandomNumbers(int[] array, int number){
         Random rand = new Random();
         for(int x = 0; x < array.length; x++){
@@ -121,6 +172,15 @@ public class Assignment12Part1 {
         }
     }
 
+    /**
+     * A method for sampling random pixels in an image.
+     * The average RGB value is taken and written to the result.
+     *
+     * @param image a file with the appropriate name
+     * @param arrayWidth An array of numbers that stores the width (like the x-coordinate)
+     * @param arrayHeight An array of numbers that stores the height (like the y-coordinate)
+     * @param result an array of numbers that stores the average RGB values
+     */
     private void getRandomPixels(BufferedImage image, int[] arrayWidth, int[] arrayHeight, double[] result){
         if(arrayHeight.length != arrayWidth.length || arrayHeight.length != result.length){
             throw new IllegalArgumentException("Arrays must have the same length");
@@ -133,6 +193,16 @@ public class Assignment12Part1 {
         }
     }
 
+    /**
+     * Based on an array containing average RGB values,
+     * it is determined which value occurs most frequently.
+     *
+     * I took this approach, where you need to determine the largest value,
+     * and with an error, as a ready-made solution.
+     *
+     * @param randomPixels an array of numbers that stores the average RGB values
+     * @return the number that occurs most often (which will be the background color)
+     */
     private double calculateIntensity(double[] randomPixels){
         return Arrays.stream(randomPixels)
                 .boxed()
@@ -146,6 +216,16 @@ public class Assignment12Part1 {
                 .orElse(255.0);
     }
 
+    /**
+     * Recursively traverses all connected pixels of the object (silhouette).
+     * It uses the DFS algorithm to calculate the total area of ​​the object in pixels.
+     *
+     * @param x The X coordinate of the current pixel.
+     * @param y The Y coordinate of the current pixel.
+     * @param isVisited An array to keep track of pixels that have already been processed.
+     * @param isBackground An array mask, where true means the background and false means part of the silhouette.
+     * @return The number of pixels found that belong to a single object.
+     */
     private int runDFSAlgorithm(int x, int y, boolean[][] isVisited, boolean[][] isBackground) {
         if (y < 0 || y >= isBackground.length || x < 0 || x >= isBackground[0].length) {
             return 0;
@@ -161,7 +241,5 @@ public class Assignment12Part1 {
                 + runDFSAlgorithm(x - 1, y, isVisited, isBackground)
                 + runDFSAlgorithm(x, y + 1, isVisited, isBackground)
                 + runDFSAlgorithm(x, y - 1, isVisited, isBackground);
-
     }
-
 }
