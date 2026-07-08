d22eabaa75601b4de16c79043c5dce403d8ade25
nwu1015 <marchenkov3004@gmail.com>
2026-07-08 15:20:00 +0300

    ArrayList. Added interface Iterable. In ArrayListRealization fixed some problems with static variable (now variable size is not-static). Added iterator to arrayList.

--- src/main/java/com/shpp/p2p/cs/vmarchenko/collection/ArrayListRealization.java
+++ src/main/java/com/shpp/p2p/cs/vmarchenko/collection/ArrayListRealization.java
@@ -1,71 +1,100 @@
 package com.shpp.p2p.cs.vmarchenko.collection;
 
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.function.Consumer;
+
 public class ArrayListRealization<E> implements ListRealization<E> {
     public static final int DEFAULT_CAPACITY = 10;
-    public static final int QUANTITY_FOR_EXPANSION = 5;
+    public static final int QUANTITY_FOR_EXPANSION = 2;
 
     private E[] list;
 
-    private static int index = 0;
+    private int size = 0;
 
+    @SuppressWarnings("unchecked")
     public ArrayListRealization() {
         list = (E[]) new Object[DEFAULT_CAPACITY];
     }
 
     @Override
     public void add(E element) {
-        if(index == list.length) {
-            E[] copyList = list;
-            list = (E[]) new Object[list.length + QUANTITY_FOR_EXPANSION];
-            System.arraycopy(copyList, 0, list, 0, copyList.length);
-        }
+        ensureCapacity();
 
-        list[index] = element;
-        index++;
+        list[size++] = element;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void ensureCapacity() {
+        if (size == list.length) {
+            int newCapacity = list.length * QUANTITY_FOR_EXPANSION;
+            E[] newList = (E[]) new Object[newCapacity];
+            System.arraycopy(list, 0, newList, 0, list.length);
+            list = newList;
+        }
     }
 
     @Override
     public void add(int index, E element) {
-        if(index == list.length - 1) {
-            E[] copyList = list;
-            list = (E[]) new Object[list.length + QUANTITY_FOR_EXPANSION];
-            System.arraycopy(copyList, 0, list, 0, copyList.length);
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
         }
-
-        E[] newList = (E[]) new Object[list.length];
-        if (index >= 0) System.arraycopy(list, 0, newList, 0, index);
-        newList[index] = element;
-        System.arraycopy(list, index, newList, index, list.length - index);
-
-        list = newList;
+        ensureCapacity();
+        System.arraycopy(list, index, list, index + 1, size - index);
+        list[index] = element;
+        size++;
     }
 
     @Override
     public boolean remove(Object element) {
-
-        int indexToRemove = -1;
-        E[] newList = (E[]) new Object[list.length];
-
-        for(int i = 0; i < list.length; i++) {
-            if(list[i].equals(element)) {
-                indexToRemove = i;
+        for (int i = 0; i < size; i++) {
+            if ((element == null && list[i] == null) || (element != null && element.equals(list[i]))) {
+                fastRemove(i);
+                return true;
             }
         }
-        if(indexToRemove != -1) {
-            System.arraycopy(list, 0, newList, 0, indexToRemove);
-            if (list.length - (indexToRemove + 1) >= 0)
-                System.arraycopy(list, indexToRemove + 1, newList, indexToRemove + 1, list.length - (indexToRemove + 1));
-        }
+        return false;
+    }
 
-        list = newList;
-        return true;
+    private void fastRemove(int index) {
+        int numMoved = size - index - 1;
+        if (numMoved > 0) {
+            System.arraycopy(list, index + 1, list, index, numMoved);
+        }
+        list[--size] = null;
     }
 
     @Override
     public E get(int index) {
-        if(index < 0 || index >= list.length) {
-            return null;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
         }
         return list[index];
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private int cursor = 0;
+
+            @Override
+            public boolean hasNext() {
+                return cursor < size;
+            }
+
+            @Override
+            public E next() {
+                if (cursor >= size) {
+                    throw new NoSuchElementException();
+                }
+                return list[cursor++];
+            }
+        };
+    }
+
+    @Override
+    public void forEach(Consumer<? super E> action) {
+        ListRealization.super.forEach(action);
     }
 }
--- src/main/java/com/shpp/p2p/cs/vmarchenko/collection/ListRealization.java
+++ src/main/java/com/shpp/p2p/cs/vmarchenko/collection/ListRealization.java
@@ -1,6 +1,6 @@
 package com.shpp.p2p.cs.vmarchenko.collection;
 
-public interface ListRealization<E> {
+public interface ListRealization<E> extends Iterable<E> {
     void add(E element);
 
     void add(int index, E element);
